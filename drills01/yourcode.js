1d1f72a4738a16a5d6e0b6b10a1697727bbac7dd
Roger Enand <rogerenand@gmail.com>
2019-09-23 20:06:03 -0700

    Completed Drills 1

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,46 @@
+// var testArray = [1, 5, 3, 2, 6]
+function sumArray(array){
+  var total=0;
+	for (currentIndex = 0; currentIndex<array.length;currentIndex++){
+    total = total + array[currentIndex];
+  }
+  return total;
+}
+// [1, 100, 2, 4, 5, 6]
+function fitWithinVal(array,topRange){
+  var newArray = [];
+  var curVal = 0;
+  var total = 0;
+  for (currentIndex = 0;currentIndex<array.length;currentIndex++){
+    curVal = array[currentIndex];
+    if (total+curVal>topRange){
 
-function sumArray(){
-	
+    }
+    else{
+      total+=curVal;
+      newArray.push(curVal);
+    }
+  }
+  return newArray;
+}
+// ['Clyde', 'Sue', 'Jerry', 'Jo']
+function getAllNamesShorterThan(array,topRange){
+  var newArray = [];
+  var curName = "";
+  for (currentIndex = 0;currentIndex<array.length;currentIndex++){
+    curName = array[currentIndex];
+    if (curName.length<topRange){
+      newArray.push(curName);
+    }
+    else{
+
+    }
+  }
+  return newArray;
 }
 
-function fitWithinVal(){
-
-}
-
-function getAllNamesShorterThan(){
-
-}
-
-function makeLabel(){
+function makeLabel(objPerson){
+  var sentance = objPerson.greeting + " " + objPerson.givenName + " " + objPerson.familyName + "\n" + objPerson['home address'].streetNumber + " " + objPerson['home address'].streetName + "\n" + objPerson['home address'].city + ", " + objPerson['home address'].state + " " + objPerson['home address'].zip;
+  return sentance;
 
 }
